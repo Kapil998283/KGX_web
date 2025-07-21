c59f05153f6c1c1b26d8d14c1dc74e41f3d9b478
kpposwal <kpposwal90@gmail.com>
2025-07-22 03:45:07 +0530

    Updated

--- teams/yourteams.php
+++ teams/yourteams.php
@@ -68,7 +68,6 @@ if (!empty($teams)) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Your Teams</title>
     <link rel="stylesheet" href="../assets/css/teams/team.css">
-    <link rel="stylesheet" href="../assets/css/teams/yourteams.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
 </head>
 
